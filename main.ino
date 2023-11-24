bf9e8e0d37660c385a2640e31b7dd82fa9403883
Alvin <alvinhuang9@gmail.com>
2023-11-24 12:52:13 +0000

    create file

---
+++ main.ino
@@ -0,0 +1,11 @@
+// this is compiling all the code into one project
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
