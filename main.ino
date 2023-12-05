56179a1cebb0999b97286e87fbd4da59d715bc73
Alvin <alvinhuang9@gmail.com>
2023-12-05 13:51:06 +0000

    PID tuning changes

--- main.ino
+++ main.ino
@@ -1,8 +1,15 @@
 // this is compiling all the code into one project
 #include "Encoder_funcs.h"
+#include "Motor_funcs.h"
+#include <PID_v1.h>
 
 Wheels Robot(18,19,20,21);
-float leftSpeed, rightSpeed;
+Motor LMotor(12,3,9);
+volatile float leftSpeed, rightSpeed;
+float setSpeeds[2];
+
+double Kp = 15, Ki = 600, Kd = 0;
+PID leftCTL();
 
 void setup() {
     // put your setup code here, to run once:
@@ -11,6 +18,7 @@ void setup() {
 }
 
 void loop() {
+
     // put your main code here, to run repeatedly:
     Robot.computeSpeeds();
     leftSpeed = Robot.getLeftSpeed(false);
