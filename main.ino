a936761e67ed92901cc7dd7f8a671ab43f9db9f9
Alvin <alvinhuang9@gmail.com>
2023-12-06 15:23:59 +0000

    setting up for PID

--- main.ino
+++ main.ino
@@ -1,30 +1,56 @@
 // this is compiling all the code into one project
 #include "Encoder_funcs.h"
 #include "Motor_funcs.h"
+#include "Serial_comms_funcs.h"
 #include <PID_v1.h>
 
+// set up both motors
 Motor LMotor(12,3,9);
 Motor RMotor(13,11,8);
 
-bool direction;
+// set up both encoders
+Wheels Encoders(19,18,20,21);
+
+// set up Serial Comms
+Comms Line;
+
+// variables for PID
+double setPointL, inputL, outputL;
+double setPointR, inputR, outputR;
+
+// speeds
+volatile double leftSpeed, rightSpeed;
+
+double Kp = 17, Ki = 600, Kd = 0;
+
+PID leftCTL(&inputL, &outputL, %setPointL, Kp, Ki, Kd, DIRECT);
+PID righttCTL(&inputR, &outputR, %setPointR, Kp, Ki, Kd, DIRECT);
+
 void setup() {
     // put your setup code here, to run once:
+    inputL = 0;
+    inputR = 0;
 
+    setPointL = 1;
+    setPointR = 1;
+
+    Serial.begin(9600);
+    Encoders.startTimer;
 }
 
 void loop() {
-    if (direction){
-        LMotor.changeDirection(true);
-        RMotor.changeDirection(false);
+    if (setPointL > 0){
+        LMotor.changeDirection(false);
     }
     else{
-        LMotor.changeDirection(false);
+        LMotor.changeDirection(true);
+    }
+
+    if (setPointR > 0){
         RMotor.changeDirection(true);
     }
-    LMotor.drive(100);
-    RMotor.drive(100);
-    
-    delay(2000);
-    
-    direction = !direction;
+    else{
+        RMotor.changeDirection(false);
+    }
+
 }
